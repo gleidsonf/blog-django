eb07634257a20fa14d132b0a9ba85e0d5d9be755
Gleidson Silva <gleidson.fgs@gmail.com>
2017-08-03 16:28:34 -0300

    Finalizando formulario de criacao do posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
+from django.http import HttpResponseRedirect
 from .models import Post
+from .forms import PostForm
+
 # O ponto depois de from significa o diretório atual ou
 # o aplicativo atual. Como views.py e models.py estão no
 # mesmo diretório podemos simplesmente usar . e o nome
@@ -15,3 +18,32 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            # return redirect('post_detail', pk=post.pk) #nao funcionou
+            return HttpResponseRedirect('/post/%s/' % post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            # return redirect('post_detail', pk=post.pk) #nao funcionou
+            return HttpResponseRedirect('/post/%s/' % post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
